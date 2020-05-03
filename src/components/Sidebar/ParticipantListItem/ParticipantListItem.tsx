7c3827216467360bc68f1f6fc4e08a9654c82406
Mauro <mauro@osn.global>
2020-05-03 17:45:25 -0300

    Remove customizations from Eze's birthdate

--- src/components/Sidebar/ParticipantListItem/ParticipantListItem.tsx
+++ src/components/Sidebar/ParticipantListItem/ParticipantListItem.tsx
@@ -6,25 +6,17 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import PersonIcon from '@material-ui/icons/Person';
 import ListItemText from '@material-ui/core/ListItemText';
-import CakeTwoToneIcon from '@material-ui/icons/CakeTwoTone';
-import BuildOutlinedIcon from '@material-ui/icons/BuildOutlined';
 
 type Props = {
   participant: Participant;
 };
 
 const ParticipantListItem = ({ participant }: Props) => {
-  let content = <PersonIcon />;
-
-  if (participant.identity === 'ezekemel') {
-    content = <CakeTwoToneIcon color="primary" />;
-  } else if (participant.identity === 'julen' || participant.identity === 'mauerv') {
-    content = <BuildOutlinedIcon />;
-  }
-
   return (
     <ListItem component="div" data-test="participant-list-item">
-      <ListItemIcon>{content}</ListItemIcon>
+      <ListItemIcon>
+        <PersonIcon />
+      </ListItemIcon>
       <ListItemText>{participant.identity}</ListItemText>
     </ListItem>
   );
