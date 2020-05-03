5ad5ef529dffb6421035bd9cae1f419fb6af7d7d
Mauro <mauro@osn.global>
2020-05-03 18:28:43 -0300

    Finish initial implementation of audio rooms.

--- src/components/AudioRoom/AudioParticipantInfo/AudioParticipantInfo.tsx
+++ src/components/AudioRoom/AudioParticipantInfo/AudioParticipantInfo.tsx
@@ -1,14 +1,43 @@
 import React, { ReactNode } from 'react';
 import { RemoteParticipant, LocalParticipant, AudioTrack } from 'twilio-video';
+import styled from 'styled-components';
 
 import usePublications from 'hooks/usePublications/usePublications';
 import useTrack from 'hooks/useTrack/useTrack';
 import useIsParticipantSpeaking from 'hooks/useIsParticipantSpeaking/useIsParticipantSpeaking';
+import PersonIcon from '@material-ui/icons/Person';
+import VolumeUpIcon from '@material-ui/icons/VolumeUp';
+import Typography from '@material-ui/core/Typography';
 
 type Props = {
   participant: RemoteParticipant | LocalParticipant;
   children: ReactNode;
 };
+
+const Container = styled.div`
+  display: flex;
+  left: 56px;
+  width: 300px;
+  height: 56px;
+  border-radius: 5px;
+  margin: ${props => props.theme.spacing(1)}px auto;
+  justify-content: space-between;
+  background-color: ${props => props.theme.palette.secondary.main};
+`;
+
+const LeftContent = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const RightContent = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const Name = styled(Typography)`
+  margin-left: ${props => props.theme.spacing(1)}px;
+`;
 
 export default function AudioParticipantInfo({ children, participant }: Props) {
   const publications = usePublications(participant);
@@ -17,10 +46,13 @@ export default function AudioParticipantInfo({ children, participant }: Props) {
   const isParticipantSpeaking = useIsParticipantSpeaking(track);
 
   return (
-    <div>
-      <p>{participant.identity}</p>
-      <p>{isParticipantSpeaking ? 'speaking' : 'silent'}</p>
+    <Container>
+      <LeftContent>
+        <PersonIcon fontSize="large" />
+        <Name>{participant.identity}</Name>
+      </LeftContent>
+      <RightContent>{isParticipantSpeaking && <VolumeUpIcon fontSize="large" />}</RightContent>
       {children}
-    </div>
+    </Container>
   );
 }
