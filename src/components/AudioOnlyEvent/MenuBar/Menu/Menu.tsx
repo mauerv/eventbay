9affab70d9a247497f1b3d5f8417cced51985701
Mauro <mauro@osn.global>
2020-05-12 19:11:04 -0300

    Add translations to MenuItems

--- src/components/AudioOnlyEvent/MenuBar/Menu/Menu.tsx
+++ src/components/AudioOnlyEvent/MenuBar/Menu/Menu.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useRef } from 'react';
+import { FormattedMessage } from 'react-intl';
 
 import IconButton from '@material-ui/core/IconButton';
 import MenuContainer from '@material-ui/core/Menu';
@@ -30,8 +31,12 @@ export default function Menu() {
         onClose={() => setMenuOpen(state => !state)}
         anchorEl={anchorRef.current}
       >
-        <MenuItem onClick={() => handleClick(leaveLobby)}>Logout</MenuItem>
-        <MenuItem onClick={() => handleClick(openSupportChat)}>Need Anything?</MenuItem>
+        <MenuItem onClick={() => handleClick(leaveLobby)}>
+          <FormattedMessage id="sidebar.logoutBtn" defaultMessage="Logout" />
+        </MenuItem>
+        <MenuItem onClick={() => handleClick(openSupportChat)}>
+          <FormattedMessage id="sidebar.supportBtn" defaultMessage="Need Help?" />
+        </MenuItem>
       </MenuContainer>
     </div>
   );
